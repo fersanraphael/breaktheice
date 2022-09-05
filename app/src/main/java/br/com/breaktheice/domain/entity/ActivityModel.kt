7ec573ea9d9316765968dd76c93710334e75c147
fersanraphael <fersanraphael@gmail.com>
2022-09-05 04:05:07 -0300

    Reverted back the serialized name annotation for entity parameters due to proguard obfuscation.

--- app/src/main/java/br/com/breaktheice/domain/entity/ActivityModel.kt
+++ app/src/main/java/br/com/breaktheice/domain/entity/ActivityModel.kt
@@ -3,6 +3,7 @@ package br.com.breaktheice.domain.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 /**
  * @author Raphael Santos
@@ -12,24 +13,31 @@ data class ActivityModel constructor(
     @field:PrimaryKey(autoGenerate = true)
     var _id: Int,
 
+    @SerializedName("participants")
     @field:ColumnInfo(name = "participants")
     var participants: Int,
 
+    @SerializedName("activity")
     @field:ColumnInfo(name = "activity")
     var activity: String?,
 
+    @SerializedName("key")
     @field:ColumnInfo(name = "key")
     var key: String?,
 
+    @SerializedName("link")
     @field:ColumnInfo(name = "link")
     var link: String?,
 
+    @SerializedName("type")
     @field:ColumnInfo(name = "type")
     var type: String?,
 
+    @SerializedName("accessibility")
     @field:ColumnInfo(name = "accessibility")
     var accessibility: Float,
 
+    @SerializedName("price")
     @field:ColumnInfo(name = "price")
     var price: Float
 ) {
