6b452cd8d655222a9a89770bcea116a5be685294
fersanraphael <fersanraphael@gmail.com>
2022-09-06 05:09:00 -0300

    Added binding adapter to validate activity type and set a drawable according.

--- app/src/main/java/br/com/breaktheice/commons/binding/AndroidBinding.kt
+++ app/src/main/java/br/com/breaktheice/commons/binding/AndroidBinding.kt
@@ -1,8 +1,12 @@
 package br.com.breaktheice.commons.binding
 
 import android.view.View
-import android.widget.TextView
+import androidx.annotation.DrawableRes
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
+import br.com.breaktheice.R
 import br.com.breaktheice.commons.utility.getAccessibilityText
 
 /**
@@ -22,13 +26,30 @@ object AndroidBinding {
 
     @JvmStatic
     @BindingAdapter("accessibilityText")
-    fun TextView.setAccessibilityText(accessibility: Float) {
+    fun AppCompatTextView.setAccessibilityText(accessibility: Float) {
         text = context.getAccessibilityText(accessibility)
     }
 
     @JvmStatic
-    @BindingAdapter("capFirstCharText")
-    fun TextView.setCapFirstCharText(type: String) {
+    @BindingAdapter("capitalizeFirstCharText")
+    fun AppCompatTextView.setCapitalizeFirstCharText(type: String) {
         text = type.replaceFirstChar { char -> char.uppercase() }
+    }
+
+    @JvmStatic
+    @BindingAdapter("srcType")
+    fun AppCompatImageView.setSrcType(type: String) {
+        @DrawableRes val resId: Int = when (type) {
+            "education" -> R.drawable.ic_education
+            "recreational" -> R.drawable.ic_recreational
+            "social" -> R.drawable.ic_social
+            "diy" -> R.drawable.ic_diy
+            "charity" -> R.drawable.ic_charity
+            "cooking" -> R.drawable.ic_cooking
+            "relaxation" -> R.drawable.ic_relaxation
+            "music" -> R.drawable.ic_music
+            else -> R.drawable.ic_busywork
+        }
+        setImageDrawable(ResourcesCompat.getDrawable(resources, resId, context.theme))
     }
 }
--- app/src/main/java/br/com/breaktheice/commons/utility/ContextUtility.kt
+++ app/src/main/java/br/com/breaktheice/commons/utility/ContextUtility.kt
@@ -9,14 +9,20 @@ import br.com.breaktheice.R
 
 fun Context.getAccessibilityText(accessibility: Float): String {
     return when (accessibility) {
-        in 0.0..0.29 -> {
+        in 0.0..0.19 -> {
+            getString(R.string.label_accessibility_very_easy)
+        }
+        in 0.20..0.39 -> {
             getString(R.string.label_accessibility_easy)
         }
-        in 0.3..0.59 -> {
+        in 0.40..0.59 -> {
             getString(R.string.label_accessibility_medium)
         }
-        else -> {
+        in 0.60..0.79 -> {
             getString(R.string.label_accessibility_hard)
+        }
+        else -> {
+            getString(R.string.label_accessibility_very_hard)
         }
     }
 }
